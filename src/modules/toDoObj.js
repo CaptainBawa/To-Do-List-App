473b8b684532ed7231aa0706039331272c3a2805
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-05 06:20:45 +0000

    Untrack files

---
+++ src/modules/toDoObj.js
@@ -0,0 +1,14 @@
+const toDoTask = [
+  {
+    index: 1,
+    description: 'Learn Web Pentesting',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Learn Python',
+    completed: false,
+  },
+];
+
+export default toDoTask;
