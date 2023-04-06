4975505c19553d9a7549caeb0da5e1f18be0ba06
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-06 21:08:38 +0000

    Add clear functionality to the tasks

--- src/modules/toDoObj.js
+++ src/modules/toDoObj.js
@@ -1,14 +1,14 @@
-const toDoTask = [
-  {
-    index: 1,
-    description: 'Learn Web Pentesting',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Learn Python',
-    completed: false,
-  },
-];
+// const toDoTask = [
+//   {
+//     index: 1,
+//     description: 'Learn Web Pentesting',
+//     completed: true,
+//   },
+//   {
+//     index: 2,
+//     description: 'Learn Python',
+//     completed: false,
+//   },
+// ];
 
-export default toDoTask;
+// export default toDoTask;
