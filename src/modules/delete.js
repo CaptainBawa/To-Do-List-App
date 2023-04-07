a38fdde40cab24fe9679d36871276539b4f2468b
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 09:12:05 +0000

    Simplify the code by using forEach loop and chaining operator

--- src/modules/delete.js
+++ src/modules/delete.js
@@ -2,15 +2,13 @@ import { tasks } from './add.js';
 
 export default function remove() {
   const hidden = document.querySelectorAll('.hidden-icon');
-  const taskContainer = document.querySelector('.task-container');
   const index = Array.from(hidden).indexOf(this);
-  if ((tasks[index].completed) === true) {
+
+  if (tasks[index]?.completed) {
     const del = this.parentNode;
-    taskContainer.removeChild(del);
+    del.remove();
     tasks.splice(index, 1);
-    for (let i = 0; i < tasks.length; i += 1) {
-      tasks[i].index = i + 1;
-    }
+    tasks.forEach((task, i) => { task.index = i + 1; });
     localStorage.setItem('array', JSON.stringify(tasks));
   }
 }
