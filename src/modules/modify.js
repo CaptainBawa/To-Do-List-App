cea1f874d718f84df4befb80bef42a97f0ef066d
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 08:51:55 +0000

    Simplify this function by grouping query selectors

--- src/modules/modify.js
+++ src/modules/modify.js
@@ -2,39 +2,35 @@ import { tasks } from './add.js';
 import remove from './delete.js';
 
 function modify() {
+  const userTask = document.querySelectorAll('.userTask');
+  const dotsIcon = document.querySelectorAll('.dots-icon');
+  const hidden = document.querySelectorAll('.hidden-icon');
+  const container = document.querySelectorAll('#container');
+  const body = document.querySelector('body');
+
   const modi = (event) => {
-    const userTask = document.querySelectorAll('.userTask');
-    const hidden = document.querySelectorAll('.hidden-icon');
     const index = Array.from(userTask).indexOf(this);
     hidden[index].removeEventListener('click', remove);
+
     if (event.target !== this) {
       const userValue = this.value;
-      const index = Array.from(userTask).indexOf(this);
       userTask[index].value = userValue;
       tasks[index].description = userTask[index].value;
       localStorage.setItem('array', JSON.stringify(tasks));
-      const container = document.querySelectorAll('#container');
       container[index].style.backgroundColor = '#fff';
       this.style.backgroundColor = '#fff';
-      const dotsIcon = document.querySelectorAll('.dots-icon');
-      const hidden = document.querySelectorAll('.hidden-icon');
       dotsIcon[index].style.display = 'block';
       hidden[index].style.display = 'none';
-      const body = document.querySelector('body');
       body.removeEventListener('click', modi);
       hidden[index].addEventListener('click', remove);
     } else if (event.target === this) {
-      const index = Array.from(userTask).indexOf(this);
-      const container = document.querySelectorAll('#container');
       container[index].style.backgroundColor = '#FFD700';
       this.style.backgroundColor = '#FFD700';
-      const dotsIcon = document.querySelectorAll('.dots-icon');
-      const hidden = document.querySelectorAll('.hidden-icon');
       dotsIcon[index].style.display = 'none';
       hidden[index].style.display = 'block';
     }
   };
-  const body = document.querySelector('body');
+
   body.addEventListener('click', modi);
 }
 
