9b77ddb38ee240e5af604ca159ab52a6bf1d841f
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 09:02:05 +0000

    Simplify the code by removing setTimeOut and using ternary operator

--- src/modules/status.js
+++ src/modules/status.js
@@ -2,30 +2,18 @@ import { tasks } from './add.js';
 
 const taskStatus = (checkbox) => {
   const index = Array.from(document.querySelectorAll('.check')).indexOf(checkbox);
-  const userTask = document.querySelectorAll('.userTask');
-  const hidden = document.querySelectorAll('.hidden-icon');
-  const dotsIcon = document.querySelectorAll('.dots-icon');
-  let value = false;
-  setTimeout(() => {
-    if (checkbox.checked === true) {
-      value = true;
-      tasks[index].completed = value;
-      userTask[index].style.textDecoration = 'line-through';
-      userTask[index].style.color = 'rgba(139, 134, 134, 0.8)';
-      userTask[index].disabled = true;
-      dotsIcon[index].style.display = 'none';
-      hidden[index].style.display = 'block';
-      localStorage.setItem('array', JSON.stringify(tasks));
-    } else {
-      tasks[index].completed = value;
-      userTask[index].style.textDecoration = 'none';
-      userTask[index].style.color = 'inherit';
-      userTask[index].disabled = false;
-      dotsIcon[index].style.display = 'block';
-      hidden[index].style.display = 'none';
-      localStorage.setItem('array', JSON.stringify(tasks));
-    }
-  }, 100);
+  const userTask = document.querySelectorAll('.userTask')[index];
+  const hidden = document.querySelectorAll('.hidden-icon')[index];
+  const dotsIcon = document.querySelectorAll('.dots-icon')[index];
+  const completed = checkbox.checked;
+
+  userTask.style.textDecoration = completed ? 'line-through' : 'none';
+  userTask.style.color = completed ? 'rgba(139, 134, 134, 0.8)' : 'inherit';
+  userTask.disabled = completed;
+  dotsIcon.style.display = completed ? 'none' : 'block';
+  hidden.style.display = completed ? 'block' : 'none';
+  tasks[index].completed = completed;
+  localStorage.setItem('array', JSON.stringify(tasks));
 };
 
 export default taskStatus;
