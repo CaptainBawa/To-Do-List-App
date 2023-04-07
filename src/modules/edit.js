9b38c674d3ce0b51256fcc24c42185af4703deef
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 09:06:52 +0000

    Simplify the code by moving variables outside the if statement

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -2,16 +2,16 @@ import { tasks } from './add.js';
 
 function edit(event) {
   const userTask = document.querySelectorAll('.userTask');
+  const index = Array.from(userTask).indexOf(event.target);
+  const container = document.querySelectorAll('#container');
+  const dotsIcon = document.querySelectorAll('.dots-icon');
+  const hidden = document.querySelectorAll('.hidden-icon');
+
   if (event.key === 'Enter' || event.key === 'Escape') {
-    const userValue = this.value;
-    const index = Array.from(userTask).indexOf(event.target);
-    userTask[index].value = userValue;
+    userTask[index].value = this.value;
     userTask[index].blur();
-    const container = document.querySelectorAll('#container');
     container[index].style.backgroundColor = '#fff';
     this.style.backgroundColor = '#fff';
-    const dotsIcon = document.querySelectorAll('.dots-icon');
-    const hidden = document.querySelectorAll('.hidden-icon');
     dotsIcon[index].style.display = 'block';
     hidden[index].style.display = 'none';
     tasks[index].description = userTask[index].value;
