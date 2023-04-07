c858c498c8eaca72100e0926fda1e47baaa57d7e
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 08:56:33 +0000

    Simplify the code by using forEach loop

--- src/clear.js
+++ src/clear.js
@@ -1,14 +1,12 @@
 import { tasks } from './modules/add.js';
 
 const clearCompleted = () => {
-  const taskCompleted = tasks.filter((tasks) => tasks.completed === true);
-  for (let i = 0; i < taskCompleted.length; i += 1) {
-    const index = (taskCompleted[i].index - 1) - i;
-    tasks.splice(index, 1);
-  }
-  for (let i = 0; i < tasks.length; i += 1) {
-    tasks[i].index = i + 1;
-  }
+  const filteredTasks = tasks.filter((task) => !task.completed);
+  filteredTasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  tasks.length = filteredTasks.length;
+  tasks.splice(0, tasks.length, ...filteredTasks);
   localStorage.setItem('array', JSON.stringify(tasks));
 };
 
