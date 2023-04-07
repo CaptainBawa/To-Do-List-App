7c3efe29288d72981b2a740f9256ed1f1148c0e8
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 07:21:05 +0000

    Add functionality to clear all completed task

--- src/clear.js
+++ src/clear.js
@@ -1,15 +1,15 @@
 import { tasks } from './modules/add.js';
 
-function clearCompleted() {
-  const completed = tasks.filter((task) => task.completed === true);
-  for (let i = 0; i < completed.length; i += 1) {
-    const index = completed[i].index - 1 - i;
+const clearCompleted = () => {
+  const taskCompleted = tasks.filter((tasks) => tasks.completed === true);
+  for (let i = 0; i < taskCompleted.length; i += 1) {
+    const index = (taskCompleted[i].index - 1) - i;
     tasks.splice(index, 1);
   }
   for (let i = 0; i < tasks.length; i += 1) {
     tasks[i].index = i + 1;
   }
   localStorage.setItem('array', JSON.stringify(tasks));
-}
+};
 
 export default clearCompleted;
