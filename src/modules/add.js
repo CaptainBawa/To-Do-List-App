9a6985df03f10c7eeb95b1cd9f8a1e340bd298e1
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 09:31:38 +0000

    Simplify if statement and Removed unnecessary string interpolation in the task object

--- src/modules/add.js
+++ src/modules/add.js
@@ -3,12 +3,8 @@ export const tasks = JSON.parse(localStorage.getItem('array')) || [];
 export const addTask = () => {
   const userInput = document.querySelector('.userInput');
   const val = userInput.value.trim();
-  if ((val.length !== 0)) {
-    const taskObj = {
-      description: `${userInput.value}`,
-      completed: false,
-      index: tasks.length + 1,
-    };
+  if (val) {
+    const taskObj = { description: userInput.value, completed: false, index: tasks.length + 1 };
     tasks.push(taskObj);
     localStorage.setItem('array', JSON.stringify(tasks));
     userInput.value = '';
