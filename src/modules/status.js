b72a10c5be1a36a4150c8b275f592b40add69499
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 07:27:37 +0000

    Add status functionality

--- src/modules/status.js
+++ src/modules/status.js
@@ -1,32 +1,32 @@
 import { tasks } from './add.js';
 
-const status = (checkbox) => {
-  const checkboxList = document.querySelectorAll('.check');
-  const index = Array.from(checkboxList).indexOf(checkbox);
+const taskStatus = () => {
+  const checkbox = document.querySelectorAll('.check');
+  const index = Array.from(checkbox).indexOf(this);
   const userTask = document.querySelectorAll('.userTask');
-  const hidden = document.querySelectorAll('.hidden');
-  const dots = document.querySelectorAll('.dots');
-  const value = checkbox.checked;
-
-  if (tasks && tasks.length > index && tasks[index]) {
-    tasks[index].completed = value;
-  }
-
-  if (userTask[index]) {
-    userTask[index].style.textDecoration = value ? 'line-through' : 'none';
-    userTask[index].style.color = value ? 'rgba(139, 134, 134, 0.8)' : 'inherit';
-    userTask[index].disabled = value;
-  }
-
-  if (dots[index]) {
-    dots[index].style.display = value ? 'none' : 'block';
-  }
-
-  if (hidden[index]) {
-    hidden[index].style.display = value ? 'block' : 'none';
-  }
-
-  localStorage.setItem('array', JSON.stringify(tasks));
+  const hidden = document.querySelectorAll('.hidden-icon');
+  const dotsIcon = document.querySelectorAll('.dots-icon');
+  let value = false;
+  setTimeout(() => {
+    if (this.checked === true) {
+      value = true;
+      tasks[index].completed = value;
+      userTask[index].style.textDecoration = 'line-through';
+      userTask[index].style.color = 'rgba(139, 134, 134, 0.8)';
+      userTask[index].disabled = true;
+      dotsIcon[index].style.display = 'none';
+      hidden[index].style.display = 'block';
+      localStorage.setItem('array', JSON.stringify(tasks));
+    } else {
+      tasks[index].completed = value;
+      userTask[index].style.textDecoration = 'none';
+      userTask[index].style.color = 'inherit';
+      userTask[index].disabled = false;
+      dotsIcon[index].style.display = 'block';
+      hidden[index].style.display = 'none';
+      localStorage.setItem('array', JSON.stringify(tasks));
+    }
+  }, 100);
 };
 
-export default status;
+export default taskStatus;
