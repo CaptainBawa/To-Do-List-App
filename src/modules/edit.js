591856de3ad635146493da59c6ffbf62eb545c47
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 07:25:32 +0000

    Add function to allow users edit task on the page

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,22 +1,22 @@
 import { tasks } from './add.js';
 
-const edit = (event) => {
+function edit(event) {
   const userTask = document.querySelectorAll('.userTask');
   if (event.key === 'Enter' || event.key === 'Escape') {
-    const userVal = event.target.value;
+    const userValue = this.value;
     const index = Array.from(userTask).indexOf(event.target);
-    userTask[index].value = userVal;
+    userTask[index].value = userValue;
     userTask[index].blur();
-    const special = document.querySelectorAll('#special');
-    special[index].style.backgroundColor = '#fff';
-    event.target.style.backgroundColor = '#fff';
-    const dots = document.querySelectorAll('.dots');
-    const hidden = document.querySelectorAll('.hidden');
-    dots[index].style.display = 'block';
+    const container = document.querySelectorAll('#container');
+    container[index].style.backgroundColor = '#fff';
+    this.style.backgroundColor = '#fff';
+    const dotsIcon = document.querySelectorAll('.dots-icon');
+    const hidden = document.querySelectorAll('.hidden-icon');
+    dotsIcon[index].style.display = 'block';
     hidden[index].style.display = 'none';
     tasks[index].description = userTask[index].value;
     localStorage.setItem('array', JSON.stringify(tasks));
   }
-};
+}
 
 export default edit;
