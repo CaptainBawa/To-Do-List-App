2cd50145c7657329be9b6e89aa7bbfb493abbeee
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-07 07:24:49 +0000

    Add function to allow users delete task from the page

--- src/modules/delete.js
+++ src/modules/delete.js
@@ -1,18 +1,16 @@
 import { tasks } from './add.js';
 
-export default function remove(element) {
-  if (element.classList.contains('delete')) {
-    const hidden = document.querySelectorAll('.hidden');
-    const container = document.querySelector('.task-container');
-    const index = Array.from(hidden).indexOf(element.parentNode);
-    if ((tasks[index].completed) === true) {
-      const del = element.parentNode;
-      container.removeChild(del);
-      tasks.splice(index, 1);
-      for (let i = 0; i < tasks.length; i += 1) {
-        tasks[i].index = i + 1;
-      }
-      localStorage.setItem('array', JSON.stringify(tasks));
+export default function remove() {
+  const hidden = document.querySelectorAll('.hidden-icon');
+  const taskContainer = document.querySelector('.task-container');
+  const index = Array.from(hidden).indexOf(this);
+  if ((tasks[index].completed) === true) {
+    const del = this.parentNode;
+    taskContainer.removeChild(del);
+    tasks.splice(index, 1);
+    for (let i = 0; i < tasks.length; i += 1) {
+      tasks[i].index = i + 1;
     }
+    localStorage.setItem('array', JSON.stringify(tasks));
   }
 }
